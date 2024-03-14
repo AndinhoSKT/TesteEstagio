da6455585b89817c19ec434503f4cfd0a4bff50a
Anderson <Anderson@DESKTOP-ELPT3D7>
2024-03-14 19:16:20 -0300

    Adicionar arquivos de projeto.

---
+++ TesteEstagio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program {
+    static bool IsFibonacci(int num) {
+        int a = 0, b = 1;
+        while (a < num) {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+        return a == num;
+    }
+
+    static void Main(string[] args) {
+        Console.Write("Digite um número: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (IsFibonacci(num)) {
+            Console.WriteLine($"{num} pertence à sequência de Fibonacci.");
+        }
+        else {
+            Console.WriteLine($"{num} não pertence à sequência de Fibonacci.");
+        }
+    }
+}
